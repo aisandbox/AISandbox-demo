d8e2febd306f2a7adb9137142b4184de53cf7aba
Graham <webmaster@aisandbox.dev>
2020-01-27 21:45:40 +0000

    added reset controls

--- src/main/java/dev/aisandbox/demo/maze/qlearning/MazeQLearningUI.java
+++ src/main/java/dev/aisandbox/demo/maze/qlearning/MazeQLearningUI.java
@@ -5,6 +5,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.imageio.ImageIO;
@@ -14,6 +15,8 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.logging.Logger;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/maze/q")
@@ -43,6 +46,27 @@ public class MazeQLearningUI {
         BufferedImage image = memory.getTableImage();
         response.setContentType("image/png");
         ImageIO.write(image,"png",response.getOutputStream());
+    }
+
+    @PostMapping("/setAlpha")
+    public RedirectView setAlpha(@RequestParam("learningrate") double alpha) {
+        LOG.info("Setting alpha to "+alpha);
+        memory.setLearningValue(alpha);
+        return new RedirectView("/maze/q/index.html");
+    }
+
+    @PostMapping("/setDiscount")
+    public RedirectView setDiscount(@RequestParam("discount") double discount) {
+        LOG.info("Setting discount to "+discount);
+        memory.setDiscount(discount);
+        return new RedirectView("/maze/q/index.html");
+    }
+
+    @GetMapping("/reset")
+    public RedirectView reset(@RequestParam("start") double start) {
+        LOG.info("Resetting with start value "+start);
+        memory.setStartingValue(start);
+        return new RedirectView("/maze/q/index.html");
     }
 
 }
