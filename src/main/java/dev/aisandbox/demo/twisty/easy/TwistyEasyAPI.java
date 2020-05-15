faacb835914f211fd7a92aba502ffe8c571e365d
Graham <graham-evans@users.noreply.github.com>
2020-05-15 21:47:25 +0100

    Link 2x2x2 solver

--- src/main/java/dev/aisandbox/demo/twisty/easy/TwistyEasyAPI.java
+++ src/main/java/dev/aisandbox/demo/twisty/easy/TwistyEasyAPI.java
@@ -2,6 +2,7 @@ package dev.aisandbox.demo.twisty.easy;
 
 import dev.aisandbox.demo.twisty.api.TwistyRequest;
 import dev.aisandbox.demo.twisty.api.TwistyResponse;
+import dev.aisandbox.demo.twisty.easy.easycube2x2x2.Solver2x2x2;
 import dev.aisandbox.demo.twisty.easy.easycube333.Solver3x3x3;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +20,12 @@ public class TwistyEasyAPI {
   private Random rand = new Random(System.currentTimeMillis());
 
   private final Solver3x3x3 cubeSolver3x3x3;
+  private final Solver2x2x2 cubeSolver2x2x2;
 
   @Autowired
-  public TwistyEasyAPI(Solver3x3x3 cubeSolver3x3x3) {
+  public TwistyEasyAPI(Solver3x3x3 cubeSolver3x3x3, Solver2x2x2 cubeSolver2x2x2) {
     this.cubeSolver3x3x3 = cubeSolver3x3x3;
+    this.cubeSolver2x2x2 = cubeSolver2x2x2;
   }
 
   @PostMapping(
@@ -33,6 +36,9 @@ public class TwistyEasyAPI {
     switch (req.getPuzzleType()) {
       case "Cube 3x3x3":
         moves = cubeSolver3x3x3.getMoves(req.getPuzzleType(), req.getState(), req.getMoves());
+        break;
+      case "Cube 2x2x2":
+        moves = cubeSolver2x2x2.getMoves(req.getState());
         break;
       default:
         log.warn("Can't solve puzzle '{}'",req.getPuzzleType());
